9b44309176dcb2bfc2182a6d14417ad97c55dec9
thomashuang <guitar10833@gmail.com>
2021-05-31 16:36:02 +0800

    update
    oran release file

--- example/ping_xapp.py
+++ example/ping_xapp.py
@@ -37,17 +37,134 @@ def entry(self):
         # rmr send 60000, should trigger registered callback
         val = json.dumps({"test_send": number}).encode()
         self.rmr_send(val, 60000)
+        """
+        rmr_send(self, payload, mtype, retries=100)
+        Allocates a buffer, sets payload and mtype, and sends
+        Parameters
+        ----------
+        payload: bytes
+            payload to set
+        mtype: int
+            message type
+        retries: int (optional)
+            Number of times to retry at the application level before excepting RMRFailure
+        Returns
+        -------
+        bool
+            whether or not the send worked after retries attempts
+        """
+
         number += 1
 
         # store it in SDL and read it back; delete and read
         self.sdl_set(my_ns, "ping", number)
+        """
+        ** Deprecate Warning **
+        ** Will be removed in a future function **
+        
+        sdl_set(self, namespace, key, value, usemsgpack=True)
+        Stores a key-value pair to SDL, optionally serializing the value
+        to bytes using msgpack.
+        Parameters
+        ----------
+        namespace: string
+            SDL namespace
+        key: string
+            SDL key
+        value:
+            Object or byte array to store.  See the `usemsgpack` parameter.
+        usemsgpack: boolean (optional, default is True)
+            Determines whether the value is serialized using msgpack before storing.
+            If usemsgpack is True, the msgpack function `packb` is invoked
+            on the value to yield a byte array that is then sent to SDL.
+            Stated differently, if usemsgpack is True, the value can be anything
+            that is serializable by msgpack.
+            If usemsgpack is False, the value must be bytes.
+        """
+        
         self.logger.info(self.sdl_get(my_ns, "ping"))
+        """
+        ** Deprecate Warning **
+        ** Will be removed in a future function **
+        Gets the value for the specified namespace and key from SDL,
+        optionally deserializing stored bytes using msgpack.
+        
+        sdl_get(self, namespace, key, usemsgpack=True)
+        Parameters
+        ----------
+        namespace: string
+            SDL namespace
+        key: string
+            SDL key
+        usemsgpack: boolean (optional, default is True)
+            If usemsgpack is True, the byte array stored by SDL is deserialized
+            using msgpack to yield the original object that was stored.
+            If usemsgpack is False, the byte array stored by SDL is returned
+            without further processing.
+        Returns
+        -------
+        Value
+            See the usemsgpack parameter for an explanation of the returned value type.
+            Answers None if the key is not found.
+        """
+
         self.logger.info(self.sdl_find_and_get(my_ns, "pin"))
+        """
+        ** Deprecate Warning **
+        ** Will be removed in a future function **
+        Gets all key-value pairs in the specified namespace
+        with keys that start with the specified prefix,
+        optionally deserializing stored bytes using msgpack.
+        
+        sdl_find_and_get(self, namespace, prefix, usemsgpack=True)
+        Parameters
+        ----------
+        nnamespaces: string
+           SDL namespace
+        prefix: string
+            the key prefix
+        usemsgpack: boolean (optional, default is True)
+            If usemsgpack is True, the byte array stored by SDL is deserialized
+            using msgpack to yield the original value that was stored.
+            If usemsgpack is False, the byte array stored by SDL is returned
+            without further processing.
+        Returns
+        -------
+        Dictionary of key-value pairs
+            Each key has the specified prefix.
+            The value object (its type) depends on the usemsgpack parameter,
+            but is either a Python object or raw bytes as discussed above.
+            Answers an empty dictionary if no keys matched the prefix.
+        """
+        
         self.sdl_delete(my_ns, "ping")
+        """
+        ** Deprecate Warning **
+        ** Will be removed in a future function **
+        Deletes the key-value pair with the specified key in the specified namespace.
+        
+        sdl_delete(self, namespace, key)
+        Parameters
+        ----------
+        namespace: string
+           SDL namespace
+        key: string
+            SDL key
+        """
+        
         self.logger.info(self.sdl_get(my_ns, "ping"))
 
         # rmr receive
         for (summary, sbuf) in self.rmr_get_messages():
+        """
+        rmr_get_messages(self)
+        Returns a generator iterable over all items in the queue that
+        have not yet been read by the client xapp. Each item is a tuple
+        (S, sbuf) where S is a message summary dict and sbuf is the raw
+        message. The caller MUST call rmr.rmr_free_msg(sbuf) when
+        finished with each sbuf to prevent memory leaks!
+        """    
+        
             # summary is a dict that contains bytes so we can't use json.dumps on it
             # so we have no good way to turn this into a string to use the logger unfortunately
             # print is more "verbose" than the ric logger
@@ -59,4 +176,26 @@ def entry(self):
 
 
 xapp = Xapp(entrypoint=entry, rmr_port=4564, use_fake_sdl=True)
+"""
+    Represents a generic Xapp where the client provides a single function
+    for the framework to call at startup time (instead of providing callback
+    functions by message type). The Xapp writer must implement and provide a
+    function with a loop-forever construct similar to the `run` function in
+    the `RMRXapp` class.  That function should poll to retrieve RMR messages
+    and dispatch them appropriately, poll for configuration changes, etc.
+    
+    Xapp(self, entrypoint, rmr_port=4562, rmr_wait_for_ready=True, use_fake_sdl=False)
+    Parameters
+    ----------
+    entrypoint: function
+        This function is called when the Xapp class's run method is invoked.
+        The function signature must be just function(self)
+    rmr_port: integer (optional, default is 4562)
+        Initialize RMR to listen on this port
+    rmr_wait_for_ready: boolean (optional, default is True)
+        Wait for RMR to signal ready before starting the dispatch loop
+    use_fake_sdl: boolean (optional, default is False)
+        Use an in-memory store instead of the real SDL service
+"""
+
 xapp.run()
--- example/pong_xapp.py
+++ example/pong_xapp.py
@@ -33,8 +33,42 @@ def sixtyh(self, summary, sbuf):
     print("pong 60000 handler received: {0}".format(summary))
     jpay = json.loads(summary[rmr.RMR_MS_MSG_PAYLOAD])
     self.rmr_rts(sbuf, new_payload=json.dumps({"ACK": jpay["test_send"]}).encode(), new_mtype=60001, retries=100)
-    self.rmr_free(sbuf)
+    """
+        Allows the xapp to return to sender, possibly adjusting the
+        payload and message type before doing so.  This does NOT free
+        the sbuf for the caller as the caller may wish to perform
+        multiple rts per buffer. The client needs to free.
 
+        rmr_rts(self, sbuf, new_payload=None, new_mtype=None, retries=100)
+        Parameters
+        ----------
+        sbuf: ctypes c_void_p
+             Pointer to an rmr message buffer
+        new_payload: bytes (optional)
+            New payload to set
+        new_mtype: int (optional)
+            New message type (replaces the received message)
+        retries: int (optional, default 100)
+            Number of times to retry at the application level
+        Returns
+        -------
+        bool
+            whether or not the send worked after retries attempts
+    """
+    
+    self.rmr_free(sbuf)
+    """ 
+        Frees an rmr message buffer after use
+        Note: this does not need to be a class method, self is not
+        used. However if we break it out as a function we need a home
+        for it.
+
+        rmr_free(self, sbuf)
+        Parameters
+        ----------
+        sbuf: ctypes c_void_p
+             Pointer to an rmr message buffer
+    """
 
 def defh(self, summary, sbuf):
     """default callback"""
@@ -42,7 +76,64 @@ def defh(self, summary, sbuf):
     print("pong default handler received: {0}".format(summary))
     self.rmr_free(sbuf)
 
-
 xapp = RMRXapp(default_handler=defh, post_init=post_init, use_fake_sdl=True)
+"""
+    Represents an Xapp that reacts only to RMR messages; i.e., the Xapp
+    only performs an action when a message is received.  Clients should
+    invoke the run method, which has a loop that waits for RMR messages
+    and calls the appropriate client-registered consume callback on each.
+    If environment variable CONFIG_FILE is defined, and that variable
+    contains a path to an existing file, this class polls a watcher
+    defined on that file to detect file-write events, and invokes a
+    configuration-change handler on each event. The handler is also
+    invoked at startup.  If no handler function is supplied to the
+    constructor, this class defines a default handler that only logs a
+    message.
+    
+    RMRXapp(self, default_handler, config_handler=None, rmr_port=4562, rmr_wait_for_ready=True, use_fake_sdl=False, post_init=None)
+    Parameters
+    ----------
+    default_handler: function
+        A function with the signature (summary, sbuf) to be called when a
+        message type is received for which no other handler is registered.
+    default_handler argument summary: dict
+        The RMR message summary, a dict of key-value pairs
+    default_handler argument sbuf: ctypes c_void_p
+        Pointer to an RMR message buffer. The user must call free on this when done.
+    config_handler: function (optional, default is documented above)
+        A function with the signature (json) to be called at startup and each time
+        a configuration-file change event is detected. The JSON object is read from
+        the configuration file, if the prerequisites are met.
+    config_handler argument json: dict
+        The contents of the configuration file, parsed as JSON.
+    rmr_port: integer (optional, default is 4562)
+        Initialize RMR to listen on this port
+    rmr_wait_for_ready: boolean (optional, default is True)
+        Wait for RMR to signal ready before starting the dispatch loop
+    use_fake_sdl: boolean (optional, default is False)
+        Use an in-memory store instead of the real SDL service
+    post_init: function (optional, default None)
+        Run this function after the app initializes and before the dispatch loop starts;
+        its signature should be post_init(self)
+"""
+
 xapp.register_callback(sixtyh, 60000)
+"""
+        registers this xapp to call handler(summary, buf) when an rmr message is received of type message_type
+        
+        register_callback(self, handler, message_type)
+        Parameters
+        ----------
+        handler: function
+            a function with the signature (summary, sbuf) to be called
+            when a message of type message_type is received
+        summary: dict
+            the rmr message summary
+        sbuf: ctypes c_void_p
+            Pointer to an rmr message buffer. The user must call free on this when done.
+        message:type: int
+            the message type to look for
+        Note if this method is called multiple times for a single message type, the "last one wins".
+"""
+
 xapp.run()  # will not thread by default
